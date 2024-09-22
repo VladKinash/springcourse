3b00f68cc4c2c967588fdc3f015a222a26e826d7
Vlad Kinash <vlad.kinash@icloud.com>
2024-09-22 11:00:27 +0300

    New project

---
+++ udemyproj/src/main/java/com/main/udemyproj/UdemyprojApplication.java
@@ -0,0 +1,13 @@
+package com.main.udemyproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyprojApplication.class, args);
+	}
+
+}
---
+++ udemyproj/src/test/java/com/main/udemyproj/UdemyprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.udemyproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
