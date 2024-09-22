1bc7887ae904f7e3cf921a73dde03b1c4286f9ee
Vlad Kinash <vlad.kinash@icloud.com>
2024-09-22 18:06:36 +0300

    Added json info to application properties

--- udemyproj/src/main/java/com/main/udemyproj/rest/FunRestController.java
+++ udemyproj/src/main/java/com/main/udemyproj/rest/FunRestController.java
@@ -3,7 +3,6 @@ package com.main.udemyproj.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class FunRestController {
 
@@ -11,6 +10,17 @@ public class FunRestController {
     public void sayHello() {
         System.out.print("Hello World");
     }
-    
+
+    //expose a new endpoint
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    //expose a new endpoint
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
 
 }
